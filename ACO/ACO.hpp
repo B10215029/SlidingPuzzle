755d892509e34f53fdb1f2f04e7e791f59a53a8a
B10215037 <B10215037@mail.ntust.edu.tw>
2017-04-03 18:17:41 +0800

    coverage, shortest_path, visibility fix

--- ACO/ACO.hpp
+++ ACO/ACO.hpp
@@ -13,7 +13,7 @@ class ACO
 public:
 	ACO(int n, float a, float b, float r, float q, int m, float e=0.01f);
 	~ACO();
-	void start(int from, bool mc=true);
+	void start(int from, bool mc=true); // default MONTE_CARLO is true
 	vector<int> shortestPath() { return shortest_path; }
 
 private:
@@ -26,7 +26,7 @@ private:
 	int max_iteration;
 	int current_iteration = 0;
 
-	float last_min_distance = 0;
+	float last_min_path = 0;
 	float converge_epsilon;
 
 	vector<int> shortest_path;
@@ -37,19 +37,18 @@ private:
 	{
 		float pheromone; // weight
 		float visibility; // distance
-		int from;
+		int from; // use int to represent state
 		int to;
 	} PathInfo;
 
 	vector<PathInfo> social_influence;
 
-	bool converge(float current_min_distance);
-	virtual bool isComplete(vector<PathInfo> walk) = 0; // compare with the complete status
+	virtual bool converge(float current_min_path); // min path is general usage (tree depth)
+	virtual bool isComplete(vector<PathInfo> & walk) = 0; // compare with the complete status
 	virtual int evaluate(int from); // depends on social_influence
 	virtual vector<int> toStates(int from) = 0; // combine to evaluate
-	virtual float visibility(int from, int to) = 0;
+	virtual float visibility(int from, int to) { return 1; } // depends on application, default is 1
 	virtual void updatePheromone(vector< vector<PathInfo> > & all_ants_walks);
-	float minDistance(vector< vector<PathInfo> > all_ants_walks);
 };
 
 ACO::ACO(int n, float a, float b, float r, float q, int m, float e=0.01f) {
@@ -65,8 +64,12 @@ ACO::ACO(int n, float a, float b, float r, float q, int m, float e=0.01f) {
 
 void ACO::start(int from, bool mc=true) {
 	MONTE_CARLO = mc;
-	float current_min_distance = 9999;
-	while(current_iteration++ < max_iteration && !converge(current_min_distance)) {
+	float current_min_path = 9999;
+	int mini;
+	while(current_iteration++ < max_iteration && !converge(current_min_path)) {
+
+		last_min_path = current_min_path;
+
 		vector< vector<PathInfo> > all_ants_walks;
 		FOR(i, number_of_ants) {
 			int current = from;
@@ -82,26 +85,30 @@ void ACO::start(int from, bool mc=true) {
 				walk.push_back(path);
 			}
 			all_ants_walks.push_back(walk);
+
+			if (current_min_path > walk.size()) {
+				current_min_path = walk.size();
+				mini = i;
+			}
 		}
 		updatePheromone(all_ants_walks);
-		last_min_distance = current_min_distance;
-		current_min_distance = minDistance(all_ants_walks);
+		shortest_path = all_ants_walks[mini];
 	}
 }
 
-bool ACO::converge(float current_min_distance) {
-	return fabs(last_min_distance - current_min_distance) <= converge_epsilon;
+bool ACO::converge(float current_min_path) {
+	return fabs(last_min_path - current_min_path) <= converge_epsilon;
 }
 
 int ACO::evaluate(int from) {
 	vector<int> to = toStates(from);
 	vector<float> weights;
 	float sum = 0, max = 0;
-	int maxi = 0;
+	int maxi;
 	FOR(i, to.size()) {
-		FOR(j, social_influence.size()) {
-			if (from == social_influence[j].from && to[i] == social_influence[j].to) {
-				float weight = pow(social_influence[j].pheromone, alpha) * pow(social_influence[j].visibility, beta);
+		for(const auto & sipath: social_influence) {
+			if (from == sipath.from && to[i] == sipath.to) {
+				float weight = pow(sipath.pheromone, alpha) * pow(sipath.visibility, beta);
 				weights.push_back(weight);
 				sum += weight;
 				if (weight > max) {
@@ -112,18 +119,15 @@ int ACO::evaluate(int from) {
 			}
 		}
 	}
-	if (MONTE_CARLO) {
-		srand(time(NULL));
-		if (sum == 0) {
-			return rand() % to.size();
-		}
-		else {
-			float r = (float) (rand() / (RAND_MAX / sum));
-			FOR(i, weights.size()) {
-				r -= weights[i];
-				if (r < 0)
-					return i;
-			}
+	srand(time(NULL));
+	if (sum == 0)
+		return rand() % to.size();
+	else if (MONTE_CARLO) {
+		float r = (float) (rand() / (RAND_MAX / sum));
+		FOR(i, weights.size()) {
+			r -= weights[i];
+			if (r < 0)
+				return i;
 		}
 	}
 	return maxi;
